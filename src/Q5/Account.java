0c0794ff9432753bfb0e8dffe3f3c5034221a532
jihoLee0818 <leejiho0114@gmail.com>
2025-07-29 17:00:12 +0900

    feat: 5번 문제 풀이 (은행 입출금 시스템 구현)

--- src/Q5/Account.java
+++ src/Q5/Account.java
@@ -10,10 +10,14 @@ public class Account {
     }
 
     public void deposit(int amount) {
+        this.balance += amount;
         // TODO: 현재 잔액(balance)에 입금액(amount)을 더하세요.
     }
 
     public void withdraw(int amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+        }
         // TODO: 출금액이 잔액보다 작거나 같을 경우에만 잔액에서 출금액을 빼세요.
     }
 }
--- src/Q5/Bank.java
+++ src/Q5/Bank.java
@@ -7,6 +7,11 @@ public class Bank {
     List<Account> accounts = new ArrayList<>();
 
     public Account findAccount(String accountNumber) {
+        for (Account account : accounts) {
+            if (account.accountNumber.equals(accountNumber)) {
+                return account;
+            }
+        }
         // TODO: 반복문을 사용하여 accounts 리스트에서
         // 매개변수로 받은 accountNumber와 일치하는 Account 객체를 찾아 반환하세요.
         // 만약 일치하는 계좌가 없다면 null을 반환하세요.
