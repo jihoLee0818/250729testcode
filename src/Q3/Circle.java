579e3ef2836688fa21e44b74c98720a3925990ec
jihoLee0818 <leejiho0114@gmail.com>
2025-07-29 16:20:43 +0900

    feat: 3번 문제 풀이 (Shape 상속 및 getArea() 오버라이딩 구현)

--- src/Q3/Circle.java
+++ src/Q3/Circle.java
@@ -1,16 +1,15 @@
 package Q3;
 
-class Circle /* TODO: 상속 구현 */ {
+class Circle extends Shape {
     private double radius; // 반지름
 
     public Circle(double radius) {
         this.radius = radius;
     }
 
-//    @Override TODO: 위 상속 구현 후 주석 해제해주세요.
+    @Override
     public double getArea() {
-        // TODO: 원의 넓이를 계산하여 반환하는 코드를 작성하세요.
-        // 원 넓이 공식: 3.14 * 반지름 * 반지름
-        return 0;
+        double Area =  radius * radius * 3.14;
+        return Area;
     }
 }
